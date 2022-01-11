7f3fefb29f8d5798e5e0acd3fec17f065670006d
SampatPolireddi <sampat.polireddi@gmail.com>
2022-01-11 10:56:30 +0530

    version-1

---
+++ nltk_utils.py
@@ -0,0 +1,45 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+import numpy as np
+stemmer =PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+a="How long does shipping take?"
+#print(a)
+
+a=tokenize(a)
+#print(a)
+
+stemmed_words=[]
+for word in a:
+    stemmed_words.append(stem(word))
+
+#print(stemmed_words)
+
+# stem each word
+
+def bag_of_words(tokenized_sentence, words):
+    
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
